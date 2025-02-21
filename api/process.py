93f0a5fc28798ad2610e9e8157fffd1b627cdbab
x945 <medbnv02@gmail.com>
2025-02-21 19:15:42 +0300

    modified spacy data

--- api/process.py
+++ api/process.py
@@ -12,7 +12,7 @@ CORS(app)  # Enable CORS for cross-origin requests
 # Initialize sentiment analysis pipeline
 sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
 # Initialize spaCy for Named Entity Recognition (NER)
-nlp = spacy.load("en_core_web_md")
+nlp = spacy.load("en_core_web_sm")
 
 logging.basicConfig(level=logging.INFO)
 
