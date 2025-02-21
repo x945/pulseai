af43991dd4e9425247c4b8fe1cafcf7572914ca4
x945 <medbnv02@gmail.com>
2025-02-21 20:09:53 +0300

    removed spacy

--- api/process.py
+++ api/process.py
@@ -1,14 +1,10 @@
 from flask import Flask, request, jsonify
-import spacy
 import logging
 import json
 import re
 from textblob import TextBlob  # Import textblob for sentiment analysis
 
 app = Flask(__name__)
-
-# Initialize spaCy for Named Entity Recognition (NER)
-nlp = spacy.load("en_core_web_sm")
 
 logging.basicConfig(level=logging.INFO)
 
@@ -39,42 +35,12 @@ def process_article():
             logging.error("Error during sentiment analysis: %s", str(e))
             return jsonify({"error": "Failed to perform sentiment analysis"}), 500
 
-        # Extract Named Entities using spaCy
-        try:
-            doc = nlp(truncated_text)
-            # Filter useful entities and add to tags set
-            for ent in doc.ents:
-                if ent.label_ in ["ORG", "PERSON", "GPE", "MONEY", "PRODUCT", "NORP", "FAC", "LOC", "EVENT", "WORK_OF_ART", "LAW", "LANGUAGE"]:
-                    print('NER FOUND:', ent.text)
-                    tags.add(ent.text)
-            logging.debug("Extracted tags from named entities: %s", tags)
-        except Exception as e:
-            logging.error("Error during tags extraction: %s", str(e))
-            return jsonify({"error": "Failed to extract tags"}), 500
-
-        # Crypto Coins Detection
-        try:
-            with open("coins.json", "r") as file:
-                data = json.load(file)
-
-            crypto_memes = {coin["name"].lower() for coin in data}  # Set for fast lookup
-
-            words = text.split()
-            words_cleaned = {re.sub(r"[^\w\s]", "", word.lower()) for word in words}  # Normalize words and remove duplicates
-
-            for word in words_cleaned:
-                if word in crypto_memes and all(word.lower() != tag.lower() for tag in tags):
-                    tags.add(word.capitalize())  # Capitalize to match expected output
-        except Exception as e:
-            logging.error("Error during crypto coin detection: %s", str(e))
-            return jsonify({"error": "Failed to detect crypto coins"}), 500
 
         return jsonify({
             "sentiment": {
                 "polarity": sentiment.polarity,
                 "subjectivity": sentiment.subjectivity
-            },
-            "tags": list(tags)  # Convert set to list for JSON serialization
+            }
         })
 
     except Exception as e:
