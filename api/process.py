cded60c1ee0532ce8694a5b94177637b77732f0a
x945 <medbnv02@gmail.com>
2025-02-22 14:03:05 +0300

    ready for prod

--- api/process.py
+++ api/process.py
@@ -106,5 +106,5 @@ def index():
     return 'PulseAI Server'
 
 # Run Flask app locally
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
